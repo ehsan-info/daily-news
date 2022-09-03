92271a81cbc16e6b8a6d1cb9185ebfa9400764a9
ehsan-info <ehsanurrahman1992@gmail.com>
2022-09-03 06:55:12 +0300

    items found message displayed

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const displayList = categories => {
         const createLi = document.createElement('li');
         createLi.classList.add('nav-item', 'active');
         createLi.innerHTML = `
-        <button onclick=categoryListDetails('${category.category_id}') class="nav-link" id="" data-bs-toggle="pill"
+        <button onclick="categoryListDetails('${category.category_id}','${category.category_name}')" class="nav-link" id="" data-bs-toggle="pill"
         data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
         aria-selected="true">${category.category_name}</button>
         `;
@@ -27,54 +27,73 @@ const displayList = categories => {
 }
 loadCategoriesList();
 
-const categoryListDetails = async (catId) => {
+const categoryListDetails = async (catId, catName) => {
+    //star laoder
+    toggleSpinner(true);
     const url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
     try {
         const res = await fetch(url);
         const data = await res.json();
-        loadCategoryDetails(data.data);
+        loadCategoryDetails(data.data, catName);
     }
     catch (error) {
         console.log(error);
     }
 }
-const loadCategoryDetails = catDetails => {
+const loadCategoryDetails = (catDetails, catName) => {
     const getNewsContainerDiv = document.getElementById('news-container-id');
+    getNewsContainerDiv.innerHTML = '';
+    //display message
+    const itemFound = document.getElementById('item-found-message');
+    itemFound.classList.remove('d-none');
+    const createMessage = document.createElement('p');
+    if (catDetails.length === 0) {
+        createMessage.innerHTML = `
+            <span>No</span> items found for category <span>${catName}</span>
+            `;
+    }
+    else {
+        createMessage.innerHTML = `
+            <span>${catDetails.length}</span> items found for category <span>${catName}</span>
+            `;
+    }
+    itemFound.appendChild(createMessage);
+
     catDetails.forEach(catDetail => {
         console.log(catDetail);
+
+        const sliceDetails = inputData => inputData.length > 500 ? `${inputData.substring(0, 500)}...` : inputData;
         const createNewsRow = document.createElement('div');
         createNewsRow.classList.add('news-div-row', 'my-3', 'card', 'p-4');
         createNewsRow.innerHTML = `
         <div class="row g-3">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <img src="${catDetail.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">${catDetail.title}</h5>
+                    <p class="card-text">${sliceDetails(catDetail.details)}</p>
                     <div class="row">
                         <div class="col-3">
                             <div class="row">
                                 <div class="col-4">
-                                    <img class="img-fluid" src="images/fas-removebg-preview.png" alt="">
+                                    <img class="img-fluid" src="${catDetail.author.img}" alt="">
                                 </div>
                                 <div class="col-8">
                                     <div class="row">
                                         <div class="col-12">
-                                            <p>Jane Cooper</p>
+                                            <p>${catDetail.author.name ? catDetail.author.name : 'No Author'}</p>
                                         </div>
                                         <div class="col-12">
-                                            <p>Jan 10,2022</p>
+                                            <p class="card-text"><small class="text-muted">${catDetail.author.published_date}</small></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-3">
-                            <p><i class="fa-solid fa-eye"></i><span>1.5M</span></p>
+                            <p><i class="fa-solid fa-eye"></i><span>${catDetail.total_view ? catDetail.total_view : 'No views yet'}</span></p>
                         </div>
                         <div class="col-3"></div>
                         <div class="col-3">
@@ -87,4 +106,15 @@ const loadCategoryDetails = catDetails => {
         `;
         getNewsContainerDiv.appendChild(createNewsRow);
     });
+    //stop loader or hide loader
+    toggleSpinner(false);
+}
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    }
+    else {
+        loaderSection.classList.add('d-none');
+    }
 }
